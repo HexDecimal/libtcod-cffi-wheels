f51f05ec7f07cd1718343d18dcbe7fd0e507fde8
Kyle Stewart <4b796c65@gmail.com>
2016-09-14 20:11:06 -0700

    merge commits into a better first commit
    try to set up Linux dev files
    
    learning to shell
    
    I'll just skip this step for now
    
    change multibuild to forked version
    
    add sheband

--- config.sh
+++ config.sh
@@ -1,13 +1,11 @@
+#!/bin/bash
 # Define custom utilities
 # Test for OSX with [ -n "$IS_OSX" ]
 
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
-    if [ -n "$IS_OSX" ] {
-        brew install sdl
-    }else{
-    }
+    :
 }
 
 function run_tests {
