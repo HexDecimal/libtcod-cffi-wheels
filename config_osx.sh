5383dd882a2ac8b6811743728249ba5d06a6eda2
Kyle Stewart <4b796c65@gmail.com>
2016-09-18 12:43:24 -0700

    converted all py's to pp

--- config_osx.sh
+++ config_osx.sh
@@ -23,11 +23,11 @@ function install_macpypy {
     # "3" or "3.4" or "3.4.1".
     # sets $PYTHON_EXE variable to python executable
     local pp_version=$1 # $(fill_pyver $1)
-    local pp_build=pypy2-v$py_version-osx64
-    local pp_zip=$py_build.tar.bz2
-    local zip_path=$DOWNLOADS_SDIR/$py_zip
+    local pp_build=pypy2-v$pp_version-osx64
+    local pp_zip=$pp_build.tar.bz2
+    local zip_path=$DOWNLOADS_SDIR/$pp_zip
     mkdir -p $DOWNLOADS_SDIR
-    wget -nv $PYPY_URL/${py_zip} -P $DOWNLOADS_SDIR
+    wget -nv $PYPY_URL/${pp_zip} -P $DOWNLOADS_SDIR
     untar $zip_path
-    PYTHON_EXE=$(realpath $py_build/bin/pypy)
+    PYTHON_EXE=$(realpath $pp_build/bin/pypy)
 }
