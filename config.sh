53b3190c714d4aff5872038e0a7780946a520e6b
Kyle Stewart <4b796c65@gmail.com>
2016-09-21 20:18:41 -0700

    test and upload

--- config.sh
+++ config.sh
@@ -5,16 +5,15 @@
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
-    if [ -n "$IS_OSX" ]
-    then
+    if [ -n "$IS_OSX" ]; then
         brew install sdl
     else
-        yum -y install SDL*
-        yum -y install mesa-libGL-devel
+        yum -y install SDL* mesa-libGL-devel
     fi
 }
 
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
+    python -c "import tcod"
 }
