0aa5d5cc24b4963a588a80a6b8c22f2f34efde93
Kyle Stewart <4b796c65@gmail.com>
2016-10-16 23:56:17 -0700

    more info

--- config.sh
+++ config.sh
@@ -9,6 +9,7 @@ function repair_wheelhouse {
         if [[ $whl == *none-any.whl ]]; then  # Pure Python wheel
             if [ "$in_dir" != "$out_dir" ]; then cp $whl $out_dir; fi
         else
+            auditwheel -V
             auditwheel -vv show $whl
             auditwheel -vv repair $whl -w $out_dir/
             # Remove unfixed if writing into same directory
