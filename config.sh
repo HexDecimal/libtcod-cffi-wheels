76b1b0f69767286113e3648de6a3398741474c42
Kyle Stewart <4b796c65@gmail.com>
2017-01-09 07:43:34 -0800

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Build SDL2 on OSX.

--- config.sh
+++ config.sh
@@ -37,8 +37,7 @@ function pre_build {
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     if [[ -z "$IS_OSX" ]]; then
-        sudo apt-get update
-        sudo apt-get install -y libglu1-mesa:i386
+        sudo apt-get install -y libglu1-mesa
     fi
     python --version
     python -c "import tcod"
